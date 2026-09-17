994aacaba0d904d27b1dfacc1e749cf191c12301
Sohel0109 <sohelkhan.profile@gmail.com>
2026-09-18 01:32:13 +0530

    Initial commit

---
+++ Day-8/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+function Para(){
+  return <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero laboriosam id sequi doloremque natus necessitatibus tempora. Natus, eum eveniet laudantium placeat aliquam ut vitae ullam ex corrupti voluptatum maxime. Molestias.</p>;
+}
+function App() {
+  return (
+    <div>
+      <h1>Day 8</h1>
+      <Para />
+    </div>
+  )
+}
+
+export default App
