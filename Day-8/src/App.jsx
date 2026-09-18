a16b1ef4fa4d107fdb1543e363c06d15168caa75
Sohel0109 <sohelkhan.profile@gmail.com>
2026-09-19 04:06:38 +0530

    Update Day 8

--- Day-8/src/App.jsx
+++ Day-8/src/App.jsx
@@ -1,5 +1,5 @@
-
 import './App.css'
+import { Cards } from './Components/Cards';
 function Para(){
   return <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero laboriosam id sequi doloremque natus necessitatibus tempora. Natus, eum eveniet laudantium placeat aliquam ut vitae ullam ex corrupti voluptatum maxime. Molestias.</p>;
 }
@@ -7,7 +7,11 @@ function App() {
   return (
     <div>
       <h1>Day 8</h1>
-      <Para />
+      <Para /> <br /><br />
+      <h1> Day 9</h1>
+      <Cards title = "Card 1" description="This is the first Card" />
+      <Cards title = "Card 2" description="This is the Second Card" />
+      <Cards title = "Card 3" description="This is the Third Card" />
     </div>
   )
 }
---
+++ Day-8/src/Components/Cards.jsx
@@ -0,0 +1,8 @@
+export const Cards = ({title, description}) => {
+    return(
+        <div className="divs">
+            <h3>{title}</h3>
+            <p>{description}</p>
+        </div>
+    )
+}
